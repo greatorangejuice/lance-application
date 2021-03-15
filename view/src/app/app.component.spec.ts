295a26258a33c197042d25d947ad57d6a0e8f184
greateorangejuice <PavelWest95>
2021-03-15 15:20:57 +0300

    change npm to yarn

--- view/src/app/app.component.spec.ts
+++ view/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lance-app'`, () => {
+  it(`should have as title 'view'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lance-app');
+    expect(app.title).toEqual('view');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('lance-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('view app is running!');
   });
 });
