2a437cb1e35f29dff5289143d3ede3554608f0fb
Pavel Snigirev <psnigirev@elinext.com>
2021-04-18 15:21:49 +0300

    add port

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 import { InnerDataValidationPipe } from './utils/pipes/innerDataValidation.pipe';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3005;
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new InnerDataValidationPipe());
   app.setGlobalPrefix('api');
-  await app.listen(3005);
+  await app.listen(PORT);
 }
 bootstrap();
